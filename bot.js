f64a1c9cf3734425f5cd26222cef312dfb8b2df7
shadyssss <52051189+shadyssss@users.noreply.github.com>
2019-06-24 13:23:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("592302721910964228")
+let channel =     client.channels.get("592460253623222338")
 setInterval(function() {
 channel.send(`No One لعيون يوسف  :heart: :heart: :heart: :heart: :heart: :heart: :heart: :heart: :heart: :heart: `);
 }, 30)
